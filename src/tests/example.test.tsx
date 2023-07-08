e94e0604fecebaa9ac14e254606d6f4a587f6cfb
sumanta2 <sumantagorai.21@gmail.com>
2023-07-08 12:41:09 +0530

    bug fix

--- src/tests/example.test.tsx
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from "vitest";
-
-describe("Example Test",() => {
-    it("should pass", () => {
-        const sum = 2 + 2;
-        expect(sum).toEqual(4);
-    })
-})
